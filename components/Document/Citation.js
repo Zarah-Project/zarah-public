589bf90bb69aae1d9d6728b35d84a0b0d973f3bc
József Gábor Bóné <jozsef.bone@gmail.com>
2021-02-25 22:58:50 +0100

    fix(citation)

--- components/Document/Citation.js
+++ components/Document/Citation.js
@@ -1,79 +1,29 @@
-import {Col, Row} from "antd";
+import {Col, Row, Spin} from "antd";
 import style from "./Citation.module.css";
 import React from "react";
+import useSWR from "swr";
+import {API, fetcher} from "../../utils/api";
 
-const Citation = ({data}) => {
-  const zoteroObject = data['zotero_data'] !== "" ? JSON.parse(data['zotero_data']) : {};
-
-  const renderAuthor = () => {
-    if (zoteroObject.hasOwnProperty('creators')) {
-      const authors = zoteroObject['creators'].map((creator) => {
-        return `${creator['firstName']} ${creator['lastName']}`
-      });
-      return authors.join(', and ')
-    }
-  };
-
-  const renderDate = () => {
-    if (zoteroObject.hasOwnProperty('date')) {
-      return ` ${zoteroObject['date']}.`
-    }
-  };
-
-  const renderPublisher = () => {
-    if (zoteroObject.hasOwnProperty('publisher')) {
-      return ` ${zoteroObject['publisher']},`
-    }
-  };
-
-  const renderJournalTitle = () => {
-    if (zoteroObject.hasOwnProperty('publicationTitle')) {
-      const journal = `${zoteroObject['publicationTitle']}`;
-      const volume = `${zoteroObject['volume'] && zoteroObject['volume'] !== "" ? ` vol. ${zoteroObject['volume']}, ` : ""}`;
-      const issue = `${zoteroObject['issue'] && zoteroObject['issue'] !== "" ? `no. ${zoteroObject['issue']}, ` : ""}`;
-      return <React.Fragment> <span>{journal}</span>{volume}{issue}</React.Fragment>
-    }
-  };
-
-  const renderCitation = () => {
-    if (data.hasOwnProperty('zotero_data')) {
-      const zoteroObject = data['zotero_data'] !== "" ? JSON.parse(data['zotero_data']) : {};
-      if (zoteroObject.hasOwnProperty('publicationTitle')) {
-          return (
-            <div className={style.CitationText}>
-              {renderAuthor()}. "{data.title}"{renderJournalTitle()}{renderDate()}
-            </div>
-          )
-      }
-
-      if (zoteroObject.hasOwnProperty('bookTitle')) {
-        return (
-          <div className={style.CitationText}>
-            {renderAuthor()}. <span>{data.title}.</span>{renderPublisher()}{renderDate()}
-          </div>
-        )
-      }
-
-      return (
-        <div className={style.CitationText}>
-          {renderAuthor()}. <span>{data.title}.</span>{renderDate()}
-        </div>
-      )
-    }
-    return ""
-  };
+const Citation = ({documentID}) => {
+  const { data, error } = useSWR(document ? `${API}/document/citation/${documentID}` : null, fetcher);
 
   return (
-    <Row>
-      <Col xs={0} md={4}/>
-      <Col xs={24} md={16}>
-        <div className={style.CitationBox}>
-          <div className={style.CitationTitle}>Suggested citation</div>
-          {renderCitation()}
-        </div>
-      </Col>
-      <Col xs={0} md={4}/>
-    </Row>
+    <React.Fragment>
+      {
+        data ?
+          <div className={style.CitationBox}>
+            <dl>
+              <dt>Suggested Citation</dt>
+              <dd>
+                <div dangerouslySetInnerHTML={{ __html: data['citation']}}/>
+              </dd>
+            </dl>
+          </div>
+        : <Spin>
+            <div className={style.CitationBox} style={{height: '100px'}} />
+          </Spin>
+      }
+    </React.Fragment>
   )
 };
 
--- components/Document/Document.js
+++ components/Document/Document.js
@@ -214,7 +214,7 @@ const Document = ({data}) => {
           {displayClassificationField("Scale of repertoire actions", "activist_repertoire_scale")}
           {displayClassificationField("Format of participation", "format_of_participation")}
           {displayClassificationField("Communication and knowledge production", "knowledge_production")}
-          <Citation data={data}/>
+          <Citation documentID={data.id}/>
         </Col>
         <Col xs={0} sm={2}/>
       </Row>
