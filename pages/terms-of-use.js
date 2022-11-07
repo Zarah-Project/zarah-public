dc5827e1c7ed1b609f4aad28549edbea5ce65afd
Josh <jozsef.bone@gmail.com>
2022-11-07 21:59:21 +0100

    Update terms-of-use.js

--- pages/terms-of-use.js
+++ pages/terms-of-use.js
@@ -21,21 +21,20 @@ export default function TermsOfUse() {
               Material contained in ZARAH DB (i.e. bibliographical information, document descriptions,
               assigned keywords, reproductions of documents, transcripts of interviews, etc.) is provided by
               ZARAH researchers and a number of contributing institutions and individuals. The interviews and
-              reproductions of documents contained in ZARAH DB may only be used in compliance with the rights and
-              terms of use displayed next to the entries and as long as the appropriate credit is given. If no
-              information about rights and terms of use is displayed next to an entry, reproductions of documents
-              and interviews may only be used in accordance with the applicable laws. To request permission, and for
-              further enquiries, please contact the institution or individual that holds the original.<br/><br/>
-              Data and metadata openly accessible in ZARAH DB are published free of restrictions under the
-              terms of <a href={"https://creativecommons.org/publicdomain/zero/1.0/"} target={"_blank"}>
-              Creative Commons CC0 1.0 Universal Public Domain Dedication.</a> Users re-using data and metadata
-              published in the ZARAH DB are required to provide attribution to the data sources.<br/><br/>
-              All content (metadata, abstracts, and other descriptions) available in ZARAH DB, created by ZARAH
-              team members, as well as graphics, and the arrangements of texts, images, and graphics by the ZARAH DB
-              designers are subject to copyright protection and other protective laws. They may be used in accordance
-              with the applicable copyright laws and scientific practice, but may not be copied, distributed,
-              altered, or made accessible to third parties for commercial purposes.<br/><br/>
-              All rights reserved.
+              reproductions of documents contained in ZARAH DB may only be used in compliance with the rights
+              and terms of use displayed next to the entries and as long as the appropriate credit is given.
+              If no information about rights and terms of use is displayed next to an entry, reproductions of
+              documents and interviews may only be used in accordance with the applicable laws.
+              To request permission, and for further enquiries, please contact the institution or individual
+              that holds the original.<br/><br/>
+              All content (metadata, abstracts, and other descriptions) available in ZARAH DB, created by
+              ZARAH team members, as well as graphics, and the arrangements of texts, images, and graphics
+              by the ZARAH DB designers are subject to copyright protection and other protective laws.
+              They may be used in accordance with the applicable copyright laws and scientific practice,
+              but may not be copied, distributed, altered, or made accessible to third parties for
+              commercial purposes.<br/><br/>
+              All rights reserved.<br/><br/>
+              Updated in October 2022.<br/><br/>
               <h2>Contact</h2>
               You can contact us via <a href={"mailto:zarahdb@ceu.edu"}>zarahdb@ceu.edu</a>
             </Col>
