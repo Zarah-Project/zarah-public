398c0444074a1c3d240fc3b758efd60811e31129
Josh <jozsef.bone@gmail.com>
2023-05-02 23:10:54 +0200

    printing updates

--- components/PDFBox/PDFBox.js
+++ components/PDFBox/PDFBox.js
@@ -1,6 +1,6 @@
-import React, {useEffect, useState} from "react";
+import React from "react";
 import { Viewer, Worker } from '@react-pdf-viewer/core';
-import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
+import { toolbarPlugin } from '@react-pdf-viewer/toolbar';
 
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
@@ -8,17 +8,54 @@ import '@react-pdf-viewer/default-layout/lib/styles/index.css';
 import style from "./PDFBox.module.css";
 
 const PDFBox = ({fileURL, width='100%'}) => {
-  const defaultLayoutPluginInstance = defaultLayoutPlugin();
+  const toolbarPluginInstance = toolbarPlugin();
+  const { renderDefaultToolbar, Toolbar } = toolbarPluginInstance;
+
+  const transform = (slot) => ({
+    ...slot,
+    // These slots will be empty
+    SwitchTheme: () => <></>,
+    Open: () => <></>,
+    Download: () => <></>
+  });
 
   return (
     fileURL ?
     <Worker workerUrl="https://unpkg.com/pdfjs-dist@2.10.377/build/pdf.worker.js">
       <div style={{ width: width, marginBottom: '20px' }} className={style.PDFBoxWrapper}>
-        <Viewer
-          fileUrl={fileURL}
-          defaultScale={1.2}
-          plugins={[defaultLayoutPluginInstance,]}
-        />
+        <div
+          className="rpv-core__viewer"
+          style={{
+            border: '1px solid rgba(0, 0, 0, 0.3)',
+            display: 'flex',
+            flexDirection: 'column',
+            height: '100%',
+          }}
+        >
+          <div
+            style={{
+              alignItems: 'center',
+              backgroundColor: '#eeeeee',
+              borderBottom: '1px solid rgba(0, 0, 0, 0.1)',
+              display: 'flex',
+              padding: '4px',
+            }}
+          >
+            <Toolbar >{renderDefaultToolbar(transform)}</Toolbar>
+          </div>
+          <div
+            style={{
+              flex: 1,
+              overflow: 'hidden',
+            }}
+          >
+            <Viewer
+              fileUrl={fileURL}
+              defaultScale={1.2}
+              plugins={[toolbarPluginInstance]}
+            />
+          </div>
+        </div>
       </div>
     </Worker> : ''
   )
